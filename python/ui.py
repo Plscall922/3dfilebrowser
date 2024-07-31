4742964e3a0db4607ddcb2b4d8bf438844b9ca0b
volodymyrhaivoronskyi <nickel.ebay.com@gmail.com>
2024-07-31 23:02:12 +1000

    ui

---
+++ python/ui.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter, QFileSystemModel, QTreeView, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('3D Model Viewer')
+        self.setGeometry(100, 100, 800, 600)
+
+        splitter = QSplitter(Qt.Horizontal)
+
+        # File browser
+        self.model = QFileSystemModel()
+        self.model.setRootPath('')
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setRootIndex(self.model.index(''))
+
+        splitter.addWidget(self.tree)
+
+        # 3D Viewer placeholder
+        self.viewer = QWidget()
+        splitter.addWidget(self.viewer)
+
+        self.setCentralWidget(splitter)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
